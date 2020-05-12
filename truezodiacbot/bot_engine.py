acf8270b6c8078fb957e26b746eaef18840bd57c
ArtemErmulin <artem.ermulin@ya.ru>
2020-05-12 21:02:24 +0300

    Add empty main commands

--- truezodiacbot/bot_engine.py
+++ truezodiacbot/bot_engine.py
@@ -1,4 +1,5 @@
 from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup
 
 from .utils import get_env_vars
 
@@ -8,10 +9,52 @@ bot = TeleBot(BOT_TOKEN)
 bot.send_message(ADMIN_ID, "I'm running.")
 
 
+@bot.message_handler(commands=["start"])
+def start_message(incoming_msg):
+    uid = incoming_msg.from_user.id
+    send_start_msg(uid)
+
+
 @bot.message_handler(content_types=["text"])
 def reply(incoming_msg):
     uid = incoming_msg.from_user.id
-    text = incoming_msg.text
+    command = incoming_msg.text
 
+    if command == "Получить гороскоп":
+        show_zodiacs(uid)
+
+    elif command == "О проекте":
+        send_about(uid)
+
+    elif command == "Обновить":
+        update_horoscope(uid)
+    else:
+        bot.send_message(chat_id=uid, text=command)
+
+
+def send_start_msg(uid):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add("Получить гороскоп")
+    keyboard.row("О проекте", "Обновить")
+    start_msg = "start message"
+
+    bot.send_message(
+        chat_id=uid,
+        text=start_msg,
+        reply_markup=keyboard,
+    )
+
+
+def show_zodiacs(uid):
+    text = "zodiacs"
     bot.send_message(chat_id=uid, text=text)
 
+
+def send_about(uid):
+    text = "about"
+    bot.send_message(chat_id=uid, text=text)
+
+
+def update_horoscope(uid):
+    text = "update"
+    bot.send_message(chat_id=uid, text=text)
