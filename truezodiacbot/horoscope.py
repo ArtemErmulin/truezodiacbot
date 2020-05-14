4fc9963f829036bc215776e13bca5e8e65595e92
ArtemErmulin <artem.ermulin@ya.ru>
2020-05-14 19:38:24 +0300

    Update generating horoscope

--- truezodiacbot/horoscope.py
+++ truezodiacbot/horoscope.py
@@ -1,2 +1,34 @@
+import json
+import os
+import random
+
+from .utils import current_datetime
+
+
+SOURCE = os.path.join(os.path.dirname(__file__), "predictions.json")
+
+with open(SOURCE) as file:
+    predictions = json.load(file)
+
+
+def random_prediction(key):
+    return random.choice(predictions[key])
+
+
 def generate(zodiac):
-    return f"some 'predict' for {zodiac}"
+    title = (
+        f"<b>{zodiac} | {current_datetime()}</b>"
+        "\n\n"
+    )
+
+    body = (
+        random_prediction("first") + "\n\n" +
+        random_prediction("second") +
+        random_prediction("second_add") + "\n\n" +
+        random_prediction("third") +
+        ""
+    )
+
+    end = ""
+
+    return title + body + end
